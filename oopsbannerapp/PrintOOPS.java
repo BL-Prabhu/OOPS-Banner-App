26c5c6e4138b4cef5883ecfdb4f74ff021cb6b20
BL-Prabhu <prabhu.nagamani@bridgelabz.com>
2026-01-29 13:05:22 +0530

    [Prabhu] Added UC1 : Print OOPS to Console

---
+++ oopsbannerapp/PrintOOPS.java
@@ -0,0 +1,29 @@
+package oopsbannerapp;
+
+
+/**
+ * PrintOOPS class
+ *
+ * <p>This class demonstrates a simple Java program
+ * that prints the text "OOPS" to the console.</p>
+ *
+ * <p>It is mainly used to understand how JavaDoc comments
+ * are written for class-level documentation.</p>
+ *
+ * @author Prabhu
+ * @version 1.0
+ */
+public class PrintOOPS
+{
+    /**
+     * Main method – Entry point of the Java program.
+     *
+     * <p>This method prints "OOPS" on the console.</p>
+     *
+     * @param args command-line arguments (not used here)
+     */
+    public static void main(String[] args)
+    {
+        System.out.println("OOPS");
+    }
+}
